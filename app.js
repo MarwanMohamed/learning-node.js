448ce63668b8d59de52da4581f8dd5c3ee7a3223
marwanmohamed <marwan.mohamed5673@gmail.com>
2018-07-23 12:52:18 +0200

    remove npm moduls

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
-
 const notes = require('./notes.js');
-
 const argv = yargs.argv;
 var command = argv._[0];
 console.log('Command: ', command);
